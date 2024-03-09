7433179e2a62adebeac591ede2a66d0526a3f1bc
and199394 <andrus199394@gmail.com>
2024-03-09 21:51:45 +0700

    Add init.h Change invoke WindowProc from procedure to static method

---
+++ init.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <Commctrl.h>
+#include <OAIdl.h>
+#include "button.h"
+#include "splitbutton.h"
+#include "checkbox.h"
+#include "radiobutton.h"
